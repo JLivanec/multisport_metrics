7e2633373a224ade892e571008f1d7cb313728fa
Jackson Livanec <jlivanec@vt.edu>
2024-02-29 20:29:43 -0500

    added foreign keys

---
+++ sql_dumps/multisport_metrics_leg0.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: multisport_metrics
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `leg`
+--
+
+DROP TABLE IF EXISTS `leg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leg` (
+  `RaceName` varchar(30) NOT NULL,
+  `RaceDate` date NOT NULL,
+  `LegName` enum('swim','bike','run') NOT NULL,
+  `Distance` varchar(10) DEFAULT NULL,
+  `Elevation` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`RaceName`,`RaceDate`,`LegName`),
+  KEY `fk_racedate_leg_idx` (`RaceDate`),
+  CONSTRAINT `fk_raceinfo` FOREIGN KEY (`RaceName`, `RaceDate`) REFERENCES `race` (`RaceName`, `RaceDate`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leg`
+--
+
+LOCK TABLES `leg` WRITE;
+/*!40000 ALTER TABLE `leg` DISABLE KEYS */;
+INSERT INTO `leg` VALUES ('Blue Ridge Draft Legal','2023-10-14','swim','750','1'),('Blue Ridge Draft Legal','2023-10-14','bike','12.4','0'),('Blue Ridge Draft Legal','2023-10-14','run','3.1','0'),('CLASH Daytona Half','2023-12-02','swim','2000','1'),('CLASH Daytona Half','2023-12-02','bike','56.2','150'),('CLASH Daytona Half','2023-12-02','run','13.1','50'),('CLASH Daytona Sprint','2023-12-01','swim','750','1'),('CLASH Daytona Sprint','2023-12-01','bike','12.4','50'),('CLASH Daytona Sprint','2023-12-01','run','3.1','5'),('Giant Acorn Olympic','2023-09-23','swim','1500','1'),('Giant Acorn Olympic','2023-09-23','bike','24.8','0'),('Giant Acorn Olympic','2023-09-23','run','6.2','0'),('Patriots Half','2023-06-17','swim','1931','1'),('Patriots Half','2023-06-17','bike','56.2','0'),('Patriots Half','2023-06-17','run','13.1','0'),('Patriots Olympic','2023-06-18','swim','1500','1'),('Patriots Olympic','2023-06-18','bike','24.8','0'),('Patriots Olympic','2023-06-18','run','6.2','0'),('Patriots Sprint','2023-06-18','swim','750','1'),('Patriots Sprint','2023-06-18','bike','12.4','0'),('Patriots Sprint','2023-06-18','run','3.1','0'),('Pleasants Landing','2023-10-07','swim','1500','1'),('Pleasants Landing','2023-10-07','bike','24.8','0'),('Pleasants Landing','2023-10-07','run','6.2','0');
+/*!40000 ALTER TABLE `leg` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-29 20:29:00
---
+++ sql_dumps/multisport_metrics_transitionresults0.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: multisport_metrics
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `transitionresults`
+--
+
+DROP TABLE IF EXISTS `transitionresults`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transitionresults` (
+  `AthleteID` char(10) NOT NULL,
+  `RaceName` varchar(30) NOT NULL,
+  `RaceDate` date NOT NULL,
+  `TName` enum('T1','T2') NOT NULL,
+  `Time` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`AthleteID`,`RaceName`,`RaceDate`,`TName`),
+  KEY `rk_raceinfo_tr_idx` (`RaceName`,`RaceDate`),
+  CONSTRAINT `rk_athleteid_tr` FOREIGN KEY (`AthleteID`) REFERENCES `athlete` (`AthleteID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `rk_raceinfo_tr` FOREIGN KEY (`RaceName`, `RaceDate`) REFERENCES `race` (`RaceName`, `RaceDate`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transitionresults`
+--
+
+LOCK TABLES `transitionresults` WRITE;
+/*!40000 ALTER TABLE `transitionresults` DISABLE KEYS */;
+INSERT INTO `transitionresults` VALUES ('2469716968','Patriots Half','2023-06-17','T1','288'),('2469716968','Patriots Half','2023-06-17','T2','72'),('3261298872','Patriots Sprint','2023-06-18','T1','172'),('3261298872','Patriots Sprint','2023-06-18','T2','122'),('3761084756','Patriots Sprint','2023-06-18','T1','253'),('3761084756','Patriots Sprint','2023-06-18','T2','52'),('3937274148','Patriots Sprint','2023-06-18','T1','224'),('3937274148','Patriots Sprint','2023-06-18','T2','102'),('5485772096','Patriots Sprint','2023-06-18','T1','185'),('5485772096','Patriots Sprint','2023-06-18','T2','80'),('5713059843','CLASH Daytona Sprint','2023-12-01','T1','86'),('5713059843','CLASH Daytona Sprint','2023-12-01','T2','83'),('5713059843','Patriots Sprint','2023-06-18','T1','125'),('5713059843','Patriots Sprint','2023-06-18','T2','47'),('7239494167','CLASH Daytona Sprint','2023-12-01','T1','105'),('7239494167','CLASH Daytona Sprint','2023-12-01','T2','82'),('7239494167','Patriots Olympic','2023-06-18','T1','144'),('7239494167','Patriots Olympic','2023-06-18','T2','51'),('8559642249','CLASH Daytona Half','2023-12-02','T1','285'),('8559642249','CLASH Daytona Half','2023-12-02','T2','179'),('8980209414','Patriots Olympic','2023-06-18','T1','152'),('8980209414','Patriots Olympic','2023-06-18','T2','76'),('9146096775','Patriots Olympic','2023-06-18','T1','177'),('9146096775','Patriots Olympic','2023-06-18','T2','88'),('9170718028','Patriots Sprint','2023-06-18','T1','121'),('9170718028','Patriots Sprint','2023-06-18','T2','49'),('9523120725','CLASH Daytona Half','2023-12-02','T1','203'),('9523120725','CLASH Daytona Half','2023-12-02','T2','237'),('9523120725','Patriots Half','2023-06-17','T1','232'),('9523120725','Patriots Half','2023-06-17','T2','132'),('9740530843','Patriots Sprint','2023-06-18','T1','163'),('9740530843','Patriots Sprint','2023-06-18','T2','91');
+/*!40000 ALTER TABLE `transitionresults` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-29 20:29:00
